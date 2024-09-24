ecb3e71ece90ef914ae3c391ee21ba8b03d6ef48
anisharoy77 <royanisha701@gmail.com>
2024-09-24 14:42:20 +0530

    adding files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
-
+import { ModalAddFormComponent } from './modal-add-form/modal-add-form.component';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ContactDataService } from './contact-data.service';
+import { Router,ActivatedRoute,ParamMap } from '@angular/router';
+import { FormBuilder, FormGroup,FormControl,Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'address-book';
+  title = 'Address book';
+  public validationForm: FormGroup;
+  constructor(
+    private modalService: NgbModal, private contactService: ContactDataService, private route: ActivatedRoute, private router:Router
+    ) {
+    
+     }
+   
+      openAddModal(): void {
+        const modalRef = this.modalService.open(ModalAddFormComponent);
+        modalRef.componentInstance.contact = {
+        id:
+        this.contactService.generateUniqueId(),
+        name: '',
+        email: '',
+        mobile: '',
+        website: '',
+        address: ' '
+        };
+        
+         modalRef.result.then((result) => {
+          if (result && result.id) {
+            
+          console.log('contacts saved',result);
+          this.router.navigate(['/contacts',result.id], {relativeTo:this.route});
+          }else{
+            console.log('Error')
+          }
+          });
+        }
+        
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule,routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+import { ModalEditFormComponent } from './modal-edit-form/modal-edit-form.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ModalAddFormComponent } from './modal-add-form/modal-add-form.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponents,
+    PageNotFoundComponent,
+   ModalEditFormComponent,
+    ModalAddFormComponent,
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgbModule,
+    ReactiveFormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [ ModalEditFormComponent, ModalAddFormComponent ]
+
 })
 export class AppModule { }
